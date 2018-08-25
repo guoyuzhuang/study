9c6c118d2cfbd9f6d0502f0d073e18b13f5ca4fa
guoyuzhuang <guoyuzhuang@DESKTOP-NEDSFBS>
2018-08-26 00:26:25 +0800

    学习SpringBoot热部署方式，好像还没有太会

---
+++ Spring_Boot/07-spring-boot-hotDeploy/src/main/java/com/luokeke/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.luokeke.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+	@RequestMapping("/showPage")
+	public String showPage(Model model) {
+		model.addAttribute("msg", "this is msg message00--0  0");
+		System.out.println("彻底解决打印日志中文乱码问题");
+		return "test";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("中文乱码");
+	}
+}
