ee8dcf407df2c8aea3c84741d21a7e88967483dd
guoyuzhuang <guoyuzhuang@DESKTOP-NEDSFBS>
2018-08-27 16:29:43 +0800

    添加设置和获取session信息代码

--- Spring_Boot/01-spring-boot/src/main/java/com/luokeke/springboot/controller/HelloController.java
+++ Spring_Boot/01-spring-boot/src/main/java/com/luokeke/springboot/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.luokeke.springboot.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +12,30 @@ public class HelloController {
 	@RequestMapping("/hello")
 	@ResponseBody
 	public String hello() {
+		return "hello";
+	}
+
+	/**
+	 * 设置session
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping("/setSession")
+	@ResponseBody
+	public String setSession(HttpSession session) {
+		session.setAttribute("key", "luokeke");
+		return "hello";
+	}
+
+	/**
+	 * 获取session值并且打印
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping("/getSession")
+	@ResponseBody
+	public String getSession(HttpSession session) {
+		System.out.println("Spring Boot获取session的值：" + session.getAttribute("key"));
 		return "hello";
 	}
 }
